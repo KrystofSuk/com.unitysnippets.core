54b4161b82e4e9c1f459c04b4b0deb36798ad793
Kryštof Šuk <suk.krystof@gmail.com>
2020-05-09 04:52:39 +0200

    Material Defaulter

---
+++ Assets/Unity Snippets/Scripts/Editor/MaterialDefaultSetterEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MaterialDefaultSetter), true)]
+public class MaterialDefaultSetterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        MaterialDefaultSetter signal = (MaterialDefaultSetter)target;
+        if(GUILayout.Button("Set Default"))
+        {
+            signal.Set();
+        }
+    }
+}
+
---
+++ Assets/Unity Snippets/Scripts/Materials/MaterialDefaultSetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialDefaultSetter : MonoBehaviour
+{
+    [SerializeField] Material targetMaterial = null;
+    [SerializeField] Material defaultMaterial = null;
+
+    private void Start() {
+        Set();
+    }
+
+    public void Set(){
+        targetMaterial.CopyPropertiesFromMaterial(defaultMaterial);
+    }
+}
