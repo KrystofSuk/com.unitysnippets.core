f239c44b1d78298d2da564cb76189efcc3e96654
Kryštof Šuk <suk.krystof@gmail.com>
2020-05-09 04:32:09 +0200

    Material Modifiers Clean

--- Assets/Unity Snippets/Scripts/Materials/MaterialColorModifier.cs
+++ Assets/Unity Snippets/Scripts/Materials/MaterialColorModifier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class MaterialThreeColorModifier : MonoBehaviour
+public class MaterialColorModifier : MonoBehaviour
 {
     [SerializeField] Material material = null;
     [SerializeField] string propertyName1 = "";
@@ -12,6 +12,10 @@ public class MaterialThreeColorModifier : MonoBehaviour
     [ColorUsage(true, true)]public Color propertyValue2 = Color.white;
     [SerializeField] string propertyName3 = "";
     [ColorUsage(true, true)]public Color propertyValue3 = Color.white;
+    [SerializeField] string propertyName4 = "";
+    [ColorUsage(true, true)]public Color propertyValue4 = Color.white;
+    [SerializeField] string propertyName5 = "";
+    [ColorUsage(true, true)]public Color propertyValue5 = Color.white;
     
     void Update()
     {
@@ -20,6 +24,8 @@ public class MaterialThreeColorModifier : MonoBehaviour
             material.SetColor(propertyName1, propertyValue1);
             material.SetColor(propertyName2, propertyValue2);
             material.SetColor(propertyName3, propertyValue3);
+            material.SetColor(propertyName4, propertyValue4);
+            material.SetColor(propertyName5, propertyValue5);
         }
     }
 }
--- Assets/Unity Snippets/Scripts/Materials/MaterialNumberModifier.cs
+++ Assets/Unity Snippets/Scripts/Materials/MaterialNumberModifier.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class MaterialTwoNumberModifier : MonoBehaviour
+public class MaterialNumberModifier : MonoBehaviour
 {
     [SerializeField] Material material = null;
     [SerializeField] string propertyName1 = "";
     public float propertyValue1;
     [SerializeField] string propertyName2 = "";
     public float propertyValue2;
+    [SerializeField] string propertyName3 = "";
+    public float propertyValue3;
+    [SerializeField] string propertyName4 = "";
+    public float propertyValue4;
+    [SerializeField] string propertyName5 = "";
+    public float propertyValue5;
     
     void Update()
     {
@@ -17,6 +23,9 @@ public class MaterialTwoNumberModifier : MonoBehaviour
         {
             material.SetFloat(propertyName1, propertyValue1);
             material.SetFloat(propertyName2, propertyValue2);
+            material.SetFloat(propertyName3, propertyValue3);
+            material.SetFloat(propertyName4, propertyValue4);
+            material.SetFloat(propertyName5, propertyValue5);
         }
     }
 }
